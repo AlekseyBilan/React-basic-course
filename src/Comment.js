5cd683ea308e45739123e4dc5a79f2e1860f9d6e
AlekseyBilan <bilan.aleksey.sergeevich@gmail.com>
2018-10-21 18:04:04 +0300

    Added functionality that shows comments and a button to show or hide the current comment.

--- src/Comment.js
+++ src/Comment.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default function Comment(props) {
-    const {comment} = props;
-    return (
-        <section>
-            <h4>{comment.user}</h4>
-            <p>{comment.text}</p>
-        </section>
-    )
+export default class Comment extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        };
+
+        this.toggleOpen = this.toggleOpen.bind(this);
+    }
+
+    render(){
+        return (
+            <section>
+                {this.getCommentLayout()}
+                <button onClick={this.toggleOpen}>
+                    {(this.state.isOpen)? 'Close Comment' : 'Open Comment'}
+                </button>
+            </section>
+        )
+    }
+    getCommentLayout (){
+        const {comment} = this.props;
+        if (this.state.isOpen){
+            return (<section><h4>{comment.user}</h4><p>{comment.text}</p></section>)
+        } else {
+            return null;
+        }
+    }
+    toggleOpen(){
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
 }
--- src/CommentsList.js
+++ src/CommentsList.js
@@ -3,9 +3,10 @@ import Comment from "./Comment";
 
 export default function CommentList (props){
     const {comments} = props;
-    const commentElements = comments.map(comment => <li key={comment.id}><Comment comment = {comment}/></li>);
-
-    return(
-        <ul>{commentElements}</ul>
-    )
+    if(comments){
+        const commentElements = comments.map(comment => <li key={comment.id}><Comment comment = {comment}/></li>);
+        return  <ul>{commentElements}</ul>
+    } else {
+        return  <p>There are no comments here.</p>
+    }
 }
