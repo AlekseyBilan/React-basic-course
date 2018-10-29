77249c15c92ffaccc513e8ffb75ce9a5d2d575b0
AlekseyBilan <bilan.aleksey.sergeevich@gmail.com>
2018-10-29 19:22:31 +0200

    add simple redux flow

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react'
 import ArticleList from './ArticleList'
 import UserForm from './UserForm'
 import Filters from './Filters'
+import Counter from './Counter'
 
 class App extends Component {
     render(){
         const {articles} = this.props;
         return (
             <div>
+                <Counter/>
                 <UserForm/>
                 <Filters articles = {articles} />
                 <ArticleList articles = {articles} defaultOpenId = {articles[0].id}/>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {articles} from './fixtures'
-import App from './components/App'
+import Root from './components/Root'
 
-render(<App articles = {articles} />, document.getElementById('container'));
+render(<Root articles = {articles} />, document.getElementById('container'));
